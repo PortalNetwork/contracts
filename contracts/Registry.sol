cf0e8f08db82ac597535a9f86e0b7ef7f98f7f65
Phyrex Tsai <x01ep23i@hotmail.com>
2018-11-22 15:04:54 +0800

    Update contract

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -11,9 +11,9 @@ interface Registry {
     // Logged when the owner of a domain transfers ownership to a new account.
     event Transfer(string indexed name, string indexed protocol, address owner);
 
-    function setRegistrant(string name, string protocol, address owner) public;
-    function setTTL(string name, string protocol, uint64 ttl) public;
-    function registrant(string name, string protocol) public view returns (address);
-    function ttl(string name, string protocol) public view returns (uint64);
+    function setRegistrant(string name, string protocol, address owner) external;
+    function setTTL(string name, string protocol, uint64 ttl) external;
+    function registrant(string name, string protocol) external view returns (address);
+    function ttl(string name, string protocol) external view returns (uint64);
 
 }
