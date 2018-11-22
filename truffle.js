e73d7e021a2fcd9e89314ded46b9e6ce102986a3
Phyrex Tsai <x01ep23i@hotmail.com>
2018-11-22 19:21:44 +0800

    Update truffle

--- truffle.js
+++ truffle.js
@@ -4,6 +4,10 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new PrivateKeyProvider(process.env.PRIVATE_KEY, 'https://ropsten.infura.io'),
+      network_id: 3
     }
   }
 };
