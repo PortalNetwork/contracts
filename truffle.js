fae2a982ed9393d07d773ff9420969f6a72b2980
Phyrex Tsai <x01ep23i@hotmail.com>
2018-11-22 19:18:10 +0800

    Update truffle

--- truffle.js
+++ truffle.js
@@ -1,9 +1,15 @@
+const PrivateKeyProvider = require("truffle-privatekey-provider");
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new PrivateKeyProvider(process.env.PRIVATE_KEY, 'https://ropsten.infura.io'),
+      network_id: 3
     }
   }
 };
