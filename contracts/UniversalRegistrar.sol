f4e89c9760f6dc477b9d29d41f199582513ea3c0
Phyrex Tsai <x01ep23i@hotmail.com>
2019-01-27 19:36:47 +0800

    Update UniversalRegistrar

--- contracts/UniversalRegistrar.sol
+++ contracts/UniversalRegistrar.sol
@@ -9,12 +9,25 @@ contract UniversalRegistrar is NameRegex, ProtocolRegex {
 
     using strings for *;
 
+    enum Mode { Open, Auction, Owned, Forbidden, Reveal, NotYetAvailable }
+
     Registry registry;
 
     constructor(Registry registryAddr) public {
         registry = registryAddr;
     }
 
+    modifier inState(string _name, string _protocol) {
+        // TODO check state
+        _;
+    }
+
+    modifier onlyOwner(string _name, string _protocol) {
+        // TODO check the domain owner, msg.sender is the highest bidder
+        _;
+    }
+
+    // TODO remove this function in production mode
     function register(string _name, string _protocol, address _registrant) public {
         require(_name.toSlice().len() > 0);
         require(_protocol.toSlice().len() > 0);
@@ -26,5 +39,23 @@ contract UniversalRegistrar is NameRegex, ProtocolRegex {
         // TODO set metadata to PortalNetworkToken
     }
 
-     
+    // TODO startAuction
+    function startAuction(string _name, string _protocol, address _registrant, bytes32 _sealedBid) external {
+
+    }
+
+    // TODO revealAuction
+    function revealAction(string _name, string _protocol, uint _value, bytes32 _salt) external {
+
+    }
+
+    // TODO finalizeAuction
+    function finalizeAuction(string _name, string _protocol) external onlyOwner(_name, _protocol) {
+        // TODO check can finalize
+    }
+ 
+    // TODO entries (status check)
+    function entries(string _name, string _protocol) external view returns (Mode, address, uint, uint, uint) {
+        
+    }
 }
