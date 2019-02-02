d9a1c6766aae018893156242a133d4bffd0dbfce
Phyrex Tsai <x01ep23i@hotmail.com>
2019-02-03 00:03:13 +0800

    Add UniversalRegistry test case

--- test/UniversalRegistry.test.js
+++ test/UniversalRegistry.test.js
@@ -10,5 +10,19 @@ contract('UniversalRegistry', function (accounts) {
         console.log(err);
       }
     });
+
+    it('create new protocol', async () => {
+      try {
+        let universalRegistry = await UniversalRegistry.deployed();
+        await universalRegistry.registerProtocol('etc', accounts[0], {from: accounts[0]});
+
+        let isProtocolAvailable = await universalRegistry.isProtocolAvailable.call('etc');
+        let isProtocolAvailable2 = await universalRegistry.isProtocolAvailable.call('etf');
+        assert.equal(isProtocolAvailable, true, 'isProtocolAvailable isn\'t correct');
+        assert.equal(isProtocolAvailable2, false, 'isProtocolAvailable isn\'t correct');
+      } catch (err) {
+        console.log(err);
+      }
+    });
   });
 });
