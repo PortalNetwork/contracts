3307c8d067d11b15df32d8a0227ee394019f4879
Phyrex Tsai <x01ep23i@hotmail.com>
2019-03-27 13:06:08 +0800

    Update PortalNetworkConfig.sol

--- contracts/PortalNetworkTokenConfig.sol
+++ contracts/PortalNetworkTokenConfig.sol
@@ -7,5 +7,5 @@ contract PortalNetworkTokenConfig {
     uint8   public constant TOKEN_DECIMALS    = 18;
 
     uint256 public constant DECIMALSFACTOR    = 10**uint256(TOKEN_DECIMALS);
-    uint256 public constant TOKEN_TOTALSUPPLY = 4000000000 * DECIMALSFACTOR;
+    uint256 public constant TOKEN_TOTALSUPPLY = 1000000000 * DECIMALSFACTOR;
 }
